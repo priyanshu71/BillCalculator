fd0778627b65234d1a907cf1d51dd34cdbc88118
priyanshu71 <62823390+priyanshu71@users.noreply.github.com>
2020-03-29 17:56:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function calculateBill() {
+   
+    var name = document.getElementById("name").value;
+    var billAmount = parseFloat(document.getElementById("amount").value);
+    var tip = parseFloat(document.getElementById("tip").value);
+    var discount = parseFloat(document.getElementById("discount").value);
+    var mobileNumber = document.getElementById("contact").value;
+    
+    const gst = 0.18;
+    if(billAmount === 0)
+     {   alert("Enter Correct Amount");
+      return;
+    }
+    
+    if( name === "")
+        {
+           alert("Enter Name of Customer");
+            return;
+        }
+    
+    if (discount < 0)
+        {
+            alert("Choose Payment Method");
+            return;
+            
+        }
+        
+    if( mobileNumber.length != 10 )
+        {
+            alert("Enter Correct Mobile Number");
+            return;
+        }
+        
+    
+    var total = ((1+tip-discount + gst)*billAmount) ;
+     
+    total = total.toFixed(2);
+    
+     document.getElementById("display").style.display = "block";
+  document.getElementById("price").innerHTML = total;
+   
+}
+       
+
+
