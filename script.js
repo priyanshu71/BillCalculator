9dbc3273ec0312697e43ead455ec6ba30d1f7ac6
jaymandaviyald <71140589+jaymandaviyald@users.noreply.github.com>
2020-10-01 12:32:01 +0530

    Update script.js
    Added print function to print the bill.

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
+var total = 0
+
 function calculateBill() {
-   
+
     var name = document.getElementById("name").value;
     var billAmount = parseFloat(document.getElementById("amount").value);
     var tip = parseFloat(document.getElementById("tip").value);
     var discount = parseFloat(document.getElementById("discount").value);
     var mobileNumber = document.getElementById("contact").value;
-    
+
     const gst = 0.18;
-    if(billAmount === 0)
-     {   alert("Enter Correct Amount");
-      return;
+    if (billAmount === 0) {
+        alert("Enter Correct Amount");
+        return;
     }
-    
-    if( name === "")
-        {
-           alert("Enter Name of Customer");
-            return;
-        }
-    
-    if (discount < 0)
-        {
-            alert("Choose Payment Method");
-            return;
-            
-        }
-        
-    if( mobileNumber.length != 10 )
-        {
-            alert("Enter Correct Mobile Number");
-            return;
-        }
-        
-    
-    var total = ((1+tip-discount + gst)*billAmount) ;
-     
+
+    if (name === "") {
+        alert("Enter Name of Customer");
+        return;
+    }
+
+    if (discount < 0) {
+        alert("Choose Payment Method");
+        return;
+
+    }
+
+    if (mobileNumber.length != 10) {
+        alert("Enter Correct Mobile Number");
+        return;
+    }
+
+
+    total = ((1 + tip - discount + gst) * billAmount);
+
     total = total.toFixed(2);
-    
-     document.getElementById("display").style.display = "block";
-  document.getElementById("price").innerHTML = total;
-    document.getElementById("successTransaction").innerHTML= name + ", Thanks for Shopping !!!";
-   
+
+    document.getElementById("display").style.display = "block";
+    document.getElementById("price").innerHTML = total;
+    document.getElementById("successTransaction").innerHTML = name + ", Thanks for Shopping !!!";
 }
-       
 
-
+function printpage() {
+    window.print();
+}
